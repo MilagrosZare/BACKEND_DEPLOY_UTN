84441024c0b2a23d18162b331ec612248b861fbf
Milagros Zare <miluurosario13@gmail.com>
2025-04-01 19:49:17 -0300

    debug-cors

--- src/server.js
+++ src/server.js
@@ -1,28 +1,38 @@
 import ENVIROMENT from "./config/enviroment.config.js";
-import express from 'express'
+import express from 'express';
 import authRouter from "./routes/auth.router.js";
 import mongoose from "./config/mongoDB.config.js";
-import cors from 'cors'
+import cors from 'cors';
 import workspace_router from "./routes/workspace.router.js";
 import channelRouter from "./routes/channel.router.js";
 
-const app = express()
+const app = express();
+app.use(cors())
 
 const corsOptions = {
     origin: 'https://frontend-deploy-tp.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-}
+};
 
-console.log(ENVIROMENT.URL_FRONTEND)
-app.use(cors(corsOptions))
+console.log("Frontend URL:", ENVIROMENT.URL_FRONTEND);
 
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.use('/api/auth', authRouter)
-app.use('/api/workspaces', workspace_router)
-app.use('/api/channels', channelRouter)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://frontend-deploy-tp.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
-app.listen(ENVIROMENT.PORT, () =>{
-    console.log(`El servidor se esta ejecutando en http://localhost:${ENVIROMENT.PORT}`)
-})
+// Rutas
+app.use('/api/auth', authRouter);
+app.use('/api/workspaces', workspace_router);
+app.use('/api/channels', channelRouter);
+
+app.listen(ENVIROMENT.PORT, () => {
+    console.log(`El servidor se está ejecutando en http://localhost:${ENVIROMENT.PORT}`);
+});
